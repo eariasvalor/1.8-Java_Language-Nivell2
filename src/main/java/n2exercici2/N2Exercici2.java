b94c47191018522dbecb3f3a35fd0debbd540678
Esther Arias <eariasvalor@gmail.com>
2025-10-20 10:26:05 +0200

    feat: create classes and code

---
+++ src/main/java/n2exercici2/N2Exercici2.java
@@ -0,0 +1,16 @@
+package n2exercici2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class N2Exercici2 {
+    public static void main(String[] args){
+        List<Integer> numbers = SampleNumbers.getNumbers();
+        String result = numbers.stream()
+                .map(n -> (n % 2 == 0 ? "e" : "o") + n)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(result);
+
+    }
+}
---
+++ src/main/java/n2exercici2/SampleNumbers.java
@@ -0,0 +1,20 @@
+package n2exercici2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleNumbers {
+
+    public static List<Integer> getNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(45);
+        numbers.add(122);
+        numbers.add(7654);
+        numbers.add(1);
+        numbers.add(24);
+
+        return numbers;
+    }
+
+}
