60b35b24dc336f0e67f7cb6925f11c2172c74a82
Esther Arias <eariasvalor@gmail.com>
2025-10-20 09:51:57 +0200

    minor changes

---
+++ src/main/java/n2exercici1/Filter.java
@@ -0,0 +1,20 @@
+package n2exercici1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+    public List<String> filterByFirstLetterAndLength(List<String> list, String letter, int numChars){
+        if (list == null) {
+            throw new IllegalArgumentException("The list or the letter cannot be empty.");
+        }
+        String upperLetter = letter.toUpperCase();
+        return list.stream()
+                .filter(s -> s
+                        .length() == numChars)
+                .filter(s -> s
+                        .startsWith(upperLetter))
+                .collect(Collectors.toUnmodifiableList());
+
+    }
+}
---
+++ src/main/java/n2exercici1/SampleNames.java
@@ -0,0 +1,21 @@
+package n2exercici1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleNames {
+    public static List<String> getNames(){
+        List<String> names = new ArrayList<>();
+        names.add("Marcos");
+        names.add("Ana");
+        names.add("An");
+        names.add("Olga");
+        names.add("Ari");
+        names.add("Bruno");
+        names.add("Esther");
+        names.add("Bueno");
+
+        return names;
+    }
+}
+
